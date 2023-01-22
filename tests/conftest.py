03b416f98e845cb7dd32b3a3669251e31321fcd2
vakleks <82042917+vakleks@users.noreply.github.com>
2023-01-22 18:38:31 +0200

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+
+
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    options.add_argument('chrome')  # use 'headless' if you don't need the browser UI
+    options.add_argument('--start-maximized')
+    options.add_argument('--window-size:800,600')
+    return options
+
+
+@pytest.fixture
+def get_chromedriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+@pytest.fixture
+def setup(request, get_webdriver):
+    driver = get_webdriver
+    url = 'https://www.macys.com/'
+    if request.cls is not None:
+        request.cls.driver = driver
+    driver.get(url)
+    yield driver
+    driver.quite()
+
